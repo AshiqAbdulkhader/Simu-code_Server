4c5f92dc344aa95841a99c93e0e05f5f4a0a2944
AshiqAbdulkhader <ashiqabdulkhader@gmail.com>
2021-02-15 18:23:24 +0530

    Update compile.py

--- compile.py
+++ compile.py
@@ -18,13 +18,12 @@ def compile_python(code,f_id):
     file.close()
     op1 = subprocess.run(["python3", f_id+".py"],stdout=subprocess.PIPE).stdout.decode('utf-8')
     op2 = subprocess.run(["python3", f_id+".py"],stderr=subprocess.PIPE).stderr.decode('utf-8')
-    op2="ER\#003 "+str(op2)
+    op2="ER#003 "+str(op2)
     subprocess.call(["rm",f_id+".py"])
     if(len(op1) != 0):
         return op1
     else:
         return op2
-    
 
 def compile_c(code,f_id):
     file = open(f_id+".c", "w")
@@ -32,7 +31,7 @@ def compile_c(code,f_id):
     file.close()
     cm1err = subprocess.run(["gcc", f_id+".c", "-o", f_id+"_c"], stderr = subprocess.PIPE).stderr.decode('utf-8')
     if(len(cm1err) != 0):
-        cm1err="ER\#003 "+str(cm1err)
+        cm1err="ER#003 "+str(cm1err)
         return cm1err
     else:
         op = subprocess.run(["./"+f_id+"_c"],stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -45,7 +44,7 @@ def compile_cpp(code,f_id):
     file.close()
     cm1err = subprocess.run(["g++", f_id+".cpp", "-o", f_id+"_cpp"], stderr = subprocess.PIPE).stderr.decode('utf-8')
     if(len(cm1err) != 0):
-        cm1err="ER\#003 "+str(cm1err)
+        cm1err="ER#003 "+str(cm1err)
         return cm1err
     else:
         op = subprocess.run(["./"+f_id+"_cpp"],stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -56,12 +55,12 @@ def run(code = "", lang = None, id = 0):
     create_dir()
     f_id = str(id)
     if(code == ""):
-        return "ER\#001 Empty code"
-    if(lang=='py'):
+        return "ER#001 Empty code."
+    if(lang=='python'):
         return(compile_python(code,f_id))
-    elif(lang=='c'):
+    elif(lang=='text/x-csrc'):
         return(compile_c(code,f_id))
-    elif(lang=='cpp'):
+    elif(lang=='text/x-c++src'):
         return(compile_cpp(code,f_id))
     else:
-        return "ER\#002 Unsupported language specified."
+        return "ER#002 Please select a language."
